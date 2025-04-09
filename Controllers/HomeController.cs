db678600b81a2736bd6e027794fc8f4cd6b7a7e2
harshbl <birsinghharsh@gmail.com>
2025-04-09 10:08:53 -0600

    Added database access message on April 1, 2025 by Harshbir

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using hars_quiz4_dotnet.Models;
+
+namespace hars_quiz4_dotnet.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        ViewBag.DatabaseStatus = "Database access has already been done on April 9, 2025 by Harshbir.";
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
